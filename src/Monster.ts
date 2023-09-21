962626e89f5d9deae8e0c814b2ee6d48d76b30c9
caue peruque <caue19052003@gmail.com>
2023-09-21 14:17:50 -0300

    feat: req08 & req09

---
+++ src/Monster.ts
@@ -0,0 +1,37 @@
+import { SimpleFighter } from './Fighter';
+
+export default class Monster implements SimpleFighter {
+  private _lifePoints: number;
+  private _strength: number;
+
+  constructor(life: number, s: number) {
+    this._lifePoints = life || 85;
+    this._strength = s || 63;
+  }
+
+  get lifePoints() {
+    return this._lifePoints;
+  }
+
+  get strength() {
+    return this._strength;
+  }
+
+  receiveDamage(attackPoints: number): number {
+    if (this._lifePoints > 0) {
+      this._lifePoints -= attackPoints;
+    } else {
+      this._lifePoints -= 1;
+    }
+
+    if (this._lifePoints < 0) {
+      this._lifePoints = -1;
+    }
+
+    return this._lifePoints;
+  }
+
+  attack(enemy: SimpleFighter): void {
+    enemy.receiveDamage(this._strength);
+  }
+}
